246ad7e33a67f6db81926c22788df13e717d8cb0
Seth Eliot <seliot@arpio.io>
2025-08-09 16:29:45 -0700

    add redshift

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# misc_iac
-Catch-all bin for various workloads defined in IaC
+# Miscellaneous IaC Workloads for AWS
+Catch-all bin for various workloads defined using Infrastructure as Code (IaC)
+
+I use these for testing https://arpio.io/ - which is a fully automated disaster recovery solution for AWS workloads
+
+Feel free to use these are you wish 😁
+
+If you are looking for an EKS workload, including a functional sample application, see this: https://github.com/setheliot/eks_demo
---
+++ redshift/main.tf
@@ -0,0 +1,366 @@
+provider "aws" {
+  region = local.region
+}
+
+provider "aws" {
+  alias  = "us_east_2"
+  region = "us-east-2"
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+}
+
+data "aws_availability_zones" "available" {}
+
+locals {
+  name   = "rs-seth-09082025"
+  region = "us-west-2"
+
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  s3_prefix = "redshift/${local.name}/"
+
+  tags = {
+    Example    = local.name
+    GithubRepo = "terraform-aws-redshift"
+    GithubOrg  = "terraform-aws-modules"
+  }
+}
+
+################################################################################
+# Complete
+################################################################################
+
+module "redshift" {
+  source  = "terraform-aws-modules/redshift/aws"
+  version = "6.1.0"
+
+  cluster_identifier    = local.name
+  allow_version_upgrade = true
+  node_type             = "ra3.large"
+  number_of_nodes       = 2
+  multi_az              = false
+
+  database_name   = "mydb"
+  master_username = "mydbuser"
+  # Either provide a good master password
+  #  create_random_password = false
+  #  master_password        = "MySecretPassw0rd1!" # Do better!
+  # Or make Redshift manage it in secrets manager
+  manage_master_password = true
+
+  manage_master_password_rotation              = true
+  master_password_rotation_schedule_expression = "rate(90 days)"
+
+  encrypted   = true
+  kms_key_arn = aws_kms_key.redshift.arn
+
+  enhanced_vpc_routing   = true
+  vpc_security_group_ids = [module.security_group.security_group_id]
+  subnet_ids             = module.vpc.redshift_subnets
+
+  # Only available when using the ra3.x type
+  availability_zone_relocation_enabled = true
+
+  # snapshot_copy = {
+  #   destination_region = "us-east-2"
+  #   grant_name         = aws_redshift_snapshot_copy_grant.useast2.snapshot_copy_grant_name
+  # }
+
+  logging = {
+    bucket_name   = module.s3_logs.s3_bucket_id
+    s3_key_prefix = local.s3_prefix
+  }
+
+  # Parameter group
+  parameter_group_name        = "${local.name}-custom"
+  parameter_group_description = "Custom parameter group for ${local.name} cluster"
+  parameter_group_parameters = {
+    wlm_json_configuration = {
+      name = "wlm_json_configuration"
+      value = jsonencode([
+        {
+          query_concurrency = 15
+        }
+      ])
+    }
+    require_ssl = {
+      name  = "require_ssl"
+      value = true
+    }
+    use_fips_ssl = {
+      name  = "use_fips_ssl"
+      value = false
+    }
+    enable_user_activity_logging = {
+      name  = "enable_user_activity_logging"
+      value = true
+    }
+    max_concurrency_scaling_clusters = {
+      name  = "max_concurrency_scaling_clusters"
+      value = 3
+    }
+    enable_case_sensitive_identifier = {
+      name  = "enable_case_sensitive_identifier"
+      value = true
+    }
+  }
+  parameter_group_tags = {
+    Additional = "CustomParameterGroup"
+  }
+
+  # Subnet group
+  subnet_group_name        = "${local.name}-custom"
+  subnet_group_description = "Custom subnet group for ${local.name} cluster"
+  subnet_group_tags = {
+    Additional = "CustomSubnetGroup"
+  }
+
+  # Snapshot schedule
+  create_snapshot_schedule        = true
+  snapshot_schedule_identifier    = local.name
+  use_snapshot_identifier_prefix  = true
+  snapshot_schedule_description   = "Example snapshot schedule"
+  snapshot_schedule_definitions   = ["rate(12 hours)"]
+  snapshot_schedule_force_destroy = true
+
+  # Scheduled actions
+  create_scheduled_action_iam_role = true
+  scheduled_actions = {
+    pause = {
+      name          = "${local.name}-pause"
+      description   = "Pause cluster every night"
+      schedule      = "cron(0 05 * * ? *)"
+      pause_cluster = true
+    }
+    resize = {
+      name        = "${local.name}-resize"
+      description = "Resize cluster (demo only)"
+      schedule    = "cron(00 13 * * ? *)"
+      resize_cluster = {
+        node_type       = "ds2.xlarge"
+        number_of_nodes = 5
+      }
+    }
+    resume = {
+      name           = "${local.name}-resume"
+      description    = "Resume cluster every morning"
+      schedule       = "cron(0 12 * * ? *)"
+      resume_cluster = true
+    }
+  }
+
+  # Endpoint access - only available when using the ra3.x type
+  create_endpoint_access          = true
+  endpoint_name                   = "${local.name}-example"
+  endpoint_subnet_group_name      = aws_redshift_subnet_group.endpoint.id
+  endpoint_vpc_security_group_ids = [module.security_group.security_group_id]
+
+  # Usage limits
+  usage_limits = {
+    currency_scaling = {
+      feature_type  = "concurrency-scaling"
+      limit_type    = "time"
+      amount        = 60
+      breach_action = "emit-metric"
+    }
+    spectrum = {
+      feature_type  = "spectrum"
+      limit_type    = "data-scanned"
+      amount        = 2
+      breach_action = "disable"
+      tags = {
+        Additional = "CustomUsageLimits"
+      }
+    }
+  }
+
+  # Authentication profile
+  # authentication_profiles = {
+  #   example = {
+  #     name = "example"
+  #     content = {
+  #       AllowDBUserOverride = "1"
+  #       Client_ID           = "ExampleClientID"
+  #       App_ID              = "example"
+  #     }
+  #   }
+  #   bar = {
+  #     content = {
+  #       AllowDBUserOverride = "1"
+  #       Client_ID           = "ExampleClientID"
+  #       App_ID              = "bar"
+  #     }
+  #   }
+  # }
+
+  tags = local.tags
+}
+
+resource "aws_redshift_snapshot_copy_grant" "useast2" {
+  # Grants are declared outside of module because they are generally performed
+  # in the destination region and we do not embed multiple providers in the root module
+  provider = aws.us_east_2
+
+  snapshot_copy_grant_name = "${local.name}-us-east-2"
+  kms_key_id               = aws_kms_key.redshift_us_east_2.arn
+
+  tags = local.tags
+}
+
+################################################################################
+# Cloudwatch Logging
+################################################################################
+
+# module "with_cloudwatch_logging" {
+  # source  = "terraform-aws-modules/redshift/aws"
+  # version = "6.1.0"
+
+#   cluster_identifier = "${local.name}-with-cloudwatch-logging"
+#   node_type          = "ra3.large"
+
+#   vpc_security_group_ids = [module.security_group.security_group_id]
+#   subnet_ids             = module.vpc.redshift_subnets
+
+#   create_cloudwatch_log_group            = true
+#   cloudwatch_log_group_retention_in_days = 7
+#   logging = {
+#     log_destination_type = "cloudwatch"
+#     log_exports          = ["connectionlog", "userlog", "useractivitylog"]
+#   }
+
+#   tags = local.tags
+# }
+
+################################################################################
+# Default
+################################################################################
+
+# module "default" {
+# source  = "terraform-aws-modules/redshift/aws"
+# version = "6.1.0"
+
+#   cluster_identifier = "${local.name}-default"
+#   node_type          = "ra3.large"
+
+#   vpc_security_group_ids = [module.security_group.security_group_id]
+#   subnet_ids             = module.vpc.redshift_subnets
+
+#   tags = local.tags
+# }
+
+
+################################################################################
+# Supporting Resources
+################################################################################
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs              = local.azs
+  private_subnets  = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k)]
+  redshift_subnets = [for k, v in local.azs : cidrsubnet(local.vpc_cidr, 8, k + 10)]
+
+  # Use subnet group created by module
+  create_redshift_subnet_group = false
+
+  tags = local.tags
+}
+
+module "security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/redshift"
+  version = "~> 5.0"
+
+  name        = local.name
+  description = "Redshift security group"
+  vpc_id      = module.vpc.vpc_id
+
+  # Allow ingress rules to be accessed only within current VPC
+  ingress_rules       = ["redshift-tcp"]
+  ingress_cidr_blocks = [module.vpc.vpc_cidr_block]
+
+  # Allow all rules for all protocols
+  egress_rules = ["all-all"]
+
+  tags = local.tags
+}
+
+resource "aws_kms_key" "redshift" {
+  description             = "${local.name} - CMK for Redshift cluster"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = local.tags
+}
+
+resource "aws_kms_key" "redshift_us_east_2" {
+  provider = aws.us_east_2
+
+  description             = "${local.name} -CMK for Redshift cross-Region snapshot"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = local.tags
+}
+
+data "aws_iam_policy_document" "s3_redshift" {
+  statement {
+    sid       = "RedshiftAcl"
+    actions   = ["s3:GetBucketAcl"]
+    resources = [module.s3_logs.s3_bucket_arn]
+
+    principals {
+      type        = "Service"
+      identifiers = ["redshift.amazonaws.com"]
+    }
+  }
+
+  statement {
+    sid       = "RedshiftWrite"
+    actions   = ["s3:PutObject"]
+    resources = ["${module.s3_logs.s3_bucket_arn}/${local.s3_prefix}*"]
+    condition {
+      test     = "StringEquals"
+      values   = ["bucket-owner-full-control"]
+      variable = "s3:x-amz-acl"
+    }
+
+    principals {
+      type        = "Service"
+      identifiers = ["redshift.amazonaws.com"]
+    }
+  }
+}
+
+module "s3_logs" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket_prefix = "${local.name}-logs"
+  acl           = "log-delivery-write"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  attach_policy = true
+  policy        = data.aws_iam_policy_document.s3_redshift.json
+
+  attach_deny_insecure_transport_policy = true
+  force_destroy                         = true
+
+  tags = local.tags
+}
+
+resource "aws_redshift_subnet_group" "endpoint" {
+  name       = "${local.name}-endpoint"
+  subnet_ids = module.vpc.private_subnets
+
+  tags = local.tags
+}
